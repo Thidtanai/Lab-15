f9bd51c5984ad5a6e474b14694d995eb02d37085
Thidtanai <76035488+Thidtanai@users.noreply.github.com>
2021-02-05 17:58:05 +0700

    complete

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -32,3 +32,44 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *input,int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(input+i);
+        if(i%M == M-1) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *input,int N,int M){
+    for(int i = 0; i < N*M; i++){
+        input[i] = (double)(rand()%101)/100;
+    }
+}
+
+void findRowSum(const double *input,double *sum,int N,int M){
+  double ans =0;
+  int count = 0;
+  for(int i=0;i<N;i++){
+    for(int j=0;j<M;j++){
+      ans += *input;
+      input++;
+    }
+    sum[i] = ans;
+    ans =0;
+  }
+  while(count != 0){
+    input--;
+    count--;
+  }
+  return;
+}
+
+void findColSum(const double *a,double *b,int N,int M){
+for (int i=0 ; i<M ; i++) b[i] = 0; 
+  for(int i=0;i<M*N;i++){
+    b[i%M] += *(a+i);
+  }
+  return;
+}
